07c93f66f11b919140d37b6e06e54a3b53415314
Yury Butskevich <bxyura@gmail.com>
2023-12-03 14:08:01 +0300

    feat: add animation for burger btn on click

--- coffee-house/scripts/menu-page.js
+++ coffee-house/scripts/menu-page.js
@@ -6,8 +6,10 @@ btnBurger.addEventListener('click', () => {
   if (menu.classList.value === 'menu-list show-menu') {
     setTimeout(()=>{nav.classList.remove('nav-height');}, 990);
     menu.classList.remove('show-menu');
+    btnBurger.classList.remove('header__burger-menu_click')
   } else {
     nav.classList.add('nav-height');
     menu.classList.add('show-menu');
+    btnBurger.classList.add('header__burger-menu_click')
   }
 });
