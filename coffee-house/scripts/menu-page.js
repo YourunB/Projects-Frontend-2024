850aa788a340c1a4fcbab35af563f9d1832c5724
Yury Butskevich <bxyura@gmail.com>
2023-12-03 15:00:37 +0300

    feat: add hide menu on click Li or a in menu

--- coffee-house/scripts/menu-page.js
+++ coffee-house/scripts/menu-page.js
@@ -4,6 +4,13 @@ const body = document.body;
 const btnBurger = document.getElementById('btn-burger-menu');
 
 btnBurger.addEventListener('click', () => {
+  menu.addEventListener('click', (event) => {
+    if (event.target.nodeName === 'LI' || event.target.nodeName === 'A') {
+      menu.classList.remove('show-menu');
+      btnBurger.classList.remove('header__burger-menu_click')
+      body.classList.remove('scroll-off');
+    }
+  });
   if (menu.classList.value === 'menu-list show-menu') {
     menu.classList.remove('show-menu');
     btnBurger.classList.remove('header__burger-menu_click')
