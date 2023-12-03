3a6780799fa5b85e2818645ed055fd0df655e1dd
Yury Butskevich <bxyura@gmail.com>
2023-12-03 14:27:01 +0300

    feat: add style for scroll and scroll-off

--- coffee-house/scripts/menu-page.js
+++ coffee-house/scripts/menu-page.js
@@ -1,15 +1,16 @@
 const nav = document.getElementById('nav');
 const menu = document.getElementById('menu');
+const body = document.body;
 const btnBurger = document.getElementById('btn-burger-menu');
 
 btnBurger.addEventListener('click', () => {
   if (menu.classList.value === 'menu-list show-menu') {
-    setTimeout(()=>{nav.classList.remove('nav-height');}, 990);
     menu.classList.remove('show-menu');
     btnBurger.classList.remove('header__burger-menu_click')
+    body.classList.remove('scroll-off');
   } else {
-    nav.classList.add('nav-height');
     menu.classList.add('show-menu');
     btnBurger.classList.add('header__burger-menu_click')
+    body.classList.add('scroll-off');
   }
 });
