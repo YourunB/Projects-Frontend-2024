f392f88d5619321bb74a12e61bbfbe5990a6598e
Yury Butskevich <bxyura@gmail.com>
2024-01-12 11:56:53 +0300

    refactor: rename wrapper by wrapper-desert

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 const arrletters = ['A','B','C','D','E','F','G','H','I','J','K','L','M','N','O','P','Q','R','S','T','U','V','W','X','Y','Z'];
 
-const wrapper = document.createElement('div');
-wrapper.className = 'wrapper';
-document.body.append(wrapper);
+const wrapperDesert = document.createElement('div');
+wrapperDesert.className = 'wrapper-desert';
+document.body.append(wrapperDesert);
