4f2f71497c800082cf642ca25697fde114ad7fbd
Yury Butskevich <bxyura@gmail.com>
2024-01-12 12:13:39 +0300

    feat: add container to body for game content

--- index.js
+++ index.js
@@ -4,10 +4,14 @@ const wrapperSky = document.createElement('div');
 wrapperSky.className = 'wrapper-sky';
 document.body.append(wrapperSky);
 
+const wrapperSkySun = document.createElement('div');
+wrapperSkySun.className = 'wrapper-sky__sun';
+document.body.append(wrapperSkySun);
+
 const wrapperDesert = document.createElement('div');
 wrapperDesert.className = 'wrapper-desert';
 document.body.append(wrapperDesert);
 
-const wrapperSkySun = document.createElement('div');
-wrapperSkySun.className = 'wrapper-sky__sun';
-document.body.append(wrapperSkySun);
+const container = document.createElement('main');
+container.className = 'container';
+document.body.append(container);
