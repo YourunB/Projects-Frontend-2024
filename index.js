03406d0469f56e73c19f99600560100f261417ae
Yury Butskevich <bxyura@gmail.com>
2024-01-12 11:59:39 +0300

    feat: add wrapper-sky and animation to project

--- index.js
+++ index.js
@@ -1,5 +1,9 @@
 const arrletters = ['A','B','C','D','E','F','G','H','I','J','K','L','M','N','O','P','Q','R','S','T','U','V','W','X','Y','Z'];
 
+const wrapperSky = document.createElement('div');
+wrapperSky.className = 'wrapper-sky';
+document.body.append(wrapperSky);
+
 const wrapperDesert = document.createElement('div');
 wrapperDesert.className = 'wrapper-desert';
 document.body.append(wrapperDesert);
