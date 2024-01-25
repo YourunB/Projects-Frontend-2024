75618c73b246480cbe179b65888843ea79bb2e7a
Yury Butskevich <bxyura@gmail.com>
2024-01-25 13:22:51 +0300

    feat: add list for menu and style list

--- nonograms/index.js
+++ nonograms/index.js
@@ -7,3 +7,19 @@ header.append(title);
 
 const nav = document.createElement('nav');
 header.append(nav);
+
+const menu = document.createElement('ul');
+menu.id = 'menu';
+nav.append(menu);
+
+const menuItemGeneral = document.createElement('li');
+menuItemGeneral.textContent = 'General';
+menu.append(menuItemGeneral);
+
+const menuItemResults = document.createElement('li');
+menuItemResults.textContent = 'Results';
+menu.append(menuItemResults);
+
+const menuItemSelectLevel = document.createElement('li');
+menuItemSelectLevel.textContent = 'Select level';
+menu.append(menuItemSelectLevel);
