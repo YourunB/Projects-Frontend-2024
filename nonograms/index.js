15d6d696c4f01da38a3767e9986d836bb366cecb
Yury Butskevich <bxyura@gmail.com>
2024-01-25 12:53:09 +0300

    feat: add header, game title and nav for menu

---
+++ nonograms/index.js
@@ -0,0 +1,9 @@
+const header = document.createElement('header');
+document.body.append(header);
+
+const title = document.createElement('h1');
+title.textContent = 'Nonograms';
+header.append(title);
+
+const nav = document.createElement('nav');
+header.append(nav);
