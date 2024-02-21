8c226bde351f77ce159f52bec1e4c2ce3243809f
Yury Butskevich <bxyura@gmail.com>
2024-02-21 18:00:54 +0300

    refactor: replace data type object by interfaces in app.ts

--- news-api/src/components/app/app.ts
+++ news-api/src/components/app/app.ts
@@ -1,6 +1,27 @@
 import AppController from '../controller/controller';
 import { AppView } from '../view/appView';
 
+interface DataLoadNews {
+    articles: [{
+        source: {
+            name: string;
+            content: string;
+        };
+        title: string;
+        description: string;
+        urlToImage: string;
+        url: string;
+        author: string;
+        publishedAt: string;
+    }];
+}
+interface DataLoadSources {
+    sources: [{
+        name: string;
+        id: string;
+    }];
+}
+
 class App {
     controller: AppController;
     view: AppView;
@@ -12,8 +33,8 @@ class App {
     start() {
         const sources = document.querySelector<HTMLElement>('.sources');
         if (!sources) throw TypeError;
-        sources.addEventListener('click', (e) => this.controller.getNews(e, (data: Object) => this.view.drawNews(data)));
-        this.controller.getSources((data: Object) => this.view.drawSources(data));
+        sources.addEventListener('click', (e) => this.controller.getNews(e, (data: DataLoadNews) => this.view.drawNews(data)));
+        this.controller.getSources((data: DataLoadSources) => this.view.drawSources(data));
     }
 }
 
