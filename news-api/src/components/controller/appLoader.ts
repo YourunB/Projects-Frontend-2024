436afddea7811f37032eb42dd02a01e93e42ea6f
Yury Butskevich <bxyura@gmail.com>
2024-02-21 23:11:11 +0300

    refactor: change api key

--- news-api/src/components/controller/appLoader.ts
+++ news-api/src/components/controller/appLoader.ts
@@ -2,7 +2,7 @@ import Loader from './loader';
 
 enum ConnectApi {
     connectUrl = 'https://rss-news-api.onrender.com/',
-    connectKey = 'df9e8ba419f048b4aa842ba8ecc75bf3',
+    connectKey = '46d8662dd4a346d5b24e6cbfd2597f5d',
 }
 
 class AppLoader extends Loader {
