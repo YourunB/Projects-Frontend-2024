42f0a0f14a1bce5602c3abd9a207d3a7a619aac9
Yury Butskevich <bxyura@gmail.com>
2024-02-21 22:52:35 +0300

    refactor: add enum in appLoader

--- news-api/src/components/controller/appLoader.ts
+++ news-api/src/components/controller/appLoader.ts
@@ -1,9 +1,14 @@
 import Loader from './loader';
 
+enum ConnectApi {
+    connectUrl = 'https://rss-news-api.onrender.com/',
+    connectKey = 'df9e8ba419f048b4aa842ba8ecc75bf3',
+}
+
 class AppLoader extends Loader {
     constructor() {
-        super('https://rss-news-api.onrender.com/', {
-            apiKey: 'df9e8ba419f048b4aa842ba8ecc75bf3',
+        super(ConnectApi.connectUrl, {
+            apiKey: ConnectApi.connectKey,
         });
     }
 }
