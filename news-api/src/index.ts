86c47ee1122da077683a331015ff1096d368fc6e
Yury Butskevich <bxyura@gmail.com>
2024-02-22 19:17:16 +0300

    feat: add change theme to app

--- news-api/src/index.ts
+++ news-api/src/index.ts
@@ -4,16 +4,30 @@ import './global.css';
 const app = new App();
 app.start();
 
+setTheme();
+
 const windowSources = document.getElementById('window-sources');
+const wrapperChoose = document.getElementById('choose-box');
+const btnChoose = document.getElementById('btn-choose');
 const btnMenu = document.getElementById('btn-menu');
 const btnUp = document.getElementById('btn-to-top');
-const btnChoose = document.getElementById('btn-choose');
-const wrapperChoose = document.getElementById('choose-box');
+const btnTheme = document.getElementById('btn-theme');
 
 function showBtnUp() {
     if (document.body.getBoundingClientRect().top <= -400) btnUp?.classList.add('btn-to-top_visible');
     else btnUp?.classList.remove('btn-to-top_visible');
 }
+
+function setTheme() {
+    if (localStorage.getItem('theme') !== null) document.body.classList.add('light-theme');
+    else document.body.classList.remove('light-theme');
+}
+
+btnTheme?.addEventListener('click', () => {
+    if (localStorage.getItem('theme') === null) localStorage.setItem('theme', 'light');
+    else localStorage.removeItem('theme');
+    setTheme();
+});
 
 btnMenu?.addEventListener('click', () => {
     windowSources?.classList.toggle('sources_show');
