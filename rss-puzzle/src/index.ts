0d0e68b98c1b77c60593c828d11302af83bcd5ff
Yury Butskevich <124175026+YourunB@users.noreply.github.com>
2024-03-05 13:03:00 +0300

    feat: RSS-PZ-01 Implement Login Form (#54)
    * feat: add page for login with form logined
    
    * refactor: change button style
    
    * refactor: add autocomplete off for inputs
    
    * feat: add background image for input fields and button are clearly visible and accessible

--- rss-puzzle/src/index.ts
+++ rss-puzzle/src/index.ts
@@ -1,1 +1,25 @@
 import './global.css';
+import { pageLogin } from './pages/pageLogin';
+
+const content = document.createElement('main');
+document.body.append(content);
+
+window.addEventListener('load', () => {
+  location.hash = '#login';
+  content.append(pageLogin);
+});
+
+window.addEventListener('hashchange', () => {
+  if (location.hash === '#login') {
+    content.innerHTML = '';
+    content.append(pageLogin);
+  }
+  if (
+    location.hash !== '#login' &&
+    location.hash !== '#welcome' &&
+    location.hash !== '#game' &&
+    location.hash !== '#results'
+  ) {
+    content.innerHTML = 'This page does not exist!';
+  }
+});
