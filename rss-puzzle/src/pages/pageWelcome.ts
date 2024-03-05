9da3da6dfb237fca7c17bfce099b2fd676931601
Yury Butskevich <bxyura@gmail.com>
2024-03-05 19:33:42 +0300

    feat: add images for page welcome

--- rss-puzzle/src/pages/pageWelcome.ts
+++ rss-puzzle/src/pages/pageWelcome.ts
@@ -1,6 +1,8 @@
 import '../pages/pageWelcome.css';
 import { btnLogout } from '../components/buttonLogout';
 import { windowConfirm, windowConfirmBtnYes, windowConfirmBtnNo } from '../components/modalConfirm';
+import '../assets/images/puzzle.png';
+import '../assets/images/english.png';
 
 const pageWelcome = document.createElement('section');
 pageWelcome.classList.add('page-welcome');
@@ -24,6 +26,22 @@ const descriptionGame = document.createElement('p');
 descriptionGame.textContent =
   'RSS Puzzle is an interactive mini-game aimed at enhancing English language skills. Players assemble sentences from jumbled words, inspired by Lingualeos Phrase Constructor training. The game integrates various levels of difficulty, hint options, and a unique puzzle-like experience with artwork.';
 mainPageWelcome.append(descriptionGame);
+
+const imagePuzzle = document.createElement('img');
+imagePuzzle.classList.add('image-puzzle');
+imagePuzzle.src = 'puzzle.png';
+imagePuzzle.alt = 'Puzzle';
+mainPageWelcome.append(imagePuzzle);
+
+const wrapperImage = document.createElement('div');
+wrapperImage.classList.add('wrapper-image');
+mainPageWelcome.append(wrapperImage);
+
+const imageEnglish = document.createElement('img');
+imageEnglish.src = 'english.png';
+imageEnglish.alt = 'English';
+imageEnglish.classList.add('wrapper-image__image-english');
+wrapperImage.append(imageEnglish);
 
 btnLogout.addEventListener('click', () => {
   windowConfirm.classList.add('modal-confirm_show');
