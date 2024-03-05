eb7f39530612e8d8b541c4718d92814dfd96032e
Yury Butskevich <bxyura@gmail.com>
2024-03-05 18:53:50 +0300

    feat: add title and description game on welcome page

--- rss-puzzle/src/pages/pageWelcome.ts
+++ rss-puzzle/src/pages/pageWelcome.ts
@@ -8,12 +8,22 @@ pageWelcome.classList.add('page-welcome');
 const headerPageWelcome = document.createElement('header');
 headerPageWelcome.classList.add('page-welcome__header');
 pageWelcome.append(headerPageWelcome);
+
 headerPageWelcome.append(btnLogout);
 
 const mainPageWelcome = document.createElement('main');
 mainPageWelcome.classList.add('page-welcome__main');
 pageWelcome.append(mainPageWelcome);
 mainPageWelcome.append(windowConfirm);
+
+const titlePageWelcome = document.createElement('h1');
+titlePageWelcome.textContent = 'RSS Puzzle';
+mainPageWelcome.append(titlePageWelcome);
+
+const descriptionGame = document.createElement('p');
+descriptionGame.textContent =
+  'RSS Puzzle is an interactive mini-game aimed at enhancing English language skills. Players assemble sentences from jumbled words, inspired by Lingualeos Phrase Constructor training. The game integrates various levels of difficulty, hint options, and a unique puzzle-like experience with artwork.';
+mainPageWelcome.append(descriptionGame);
 
 btnLogout.addEventListener('click', () => {
   windowConfirm.classList.add('modal-confirm_show');
