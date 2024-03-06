4ca652f4f574c995b899c4fa84e41c515d0c5eea
Yury Butskevich <bxyura@gmail.com>
2024-03-06 13:51:08 +0300

    feat: add fetch to load data files for create puzzle cards

--- rss-puzzle/src/pages/pageGame.ts
+++ rss-puzzle/src/pages/pageGame.ts
@@ -3,4 +3,16 @@ import '../pages/pageGame.css';
 const pageGame = document.createElement('section');
 pageGame.classList.add('page-game');
 
+let arrLvls: object[] = [];
+
+function getFile(link: string) {
+  fetch(link)
+    .then((response) => response.json())
+    .then((data) => {
+      arrLvls = data;
+    });
+}
+
+getFile('lingleo/data/wordCollectionLevel1.json');
+
 export { pageGame };
