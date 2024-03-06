8e938f1041dfe3715e548433f07e76d1e8207710
Yury Butskevich <124175026+YourunB@users.noreply.github.com>
2024-03-06 18:55:16 +0300

    feat: RSS-PZ-08 Create Design Game Page With Interactive Word Cards (#62)
    * feat: add json files for puzzles cards
    
    * refactor: add CopyWebpackPlugto webpack config for save static files for puzzles cards
    
    * refactor: change pattern name branch for husky validate
    
    * feat: add fetch to load data files for create puzzle cards
    
    * feat: add game box, fileds, puzzles and add functions to create puzzles and game fields
    
    * feat: add mix words for puzzles before create puzzles
    
    * feat: add move puzzle on click and add animation for move

--- rss-puzzle/src/pages/pageGame.ts
+++ rss-puzzle/src/pages/pageGame.ts
@@ -3,4 +3,109 @@ import '../pages/pageGame.css';
 const pageGame = document.createElement('section');
 pageGame.classList.add('page-game');
 
+const headerPageGame = document.createElement('header');
+headerPageGame.classList.add('page-game__header');
+pageGame.append(headerPageGame);
+
+const mainPageGame = document.createElement('main');
+mainPageGame.classList.add('page-game__main');
+pageGame.append(mainPageGame);
+
+interface levelData {
+  audioExample: string;
+  id: number;
+  textExample: string;
+  textExampleTranslate: string;
+  word: string;
+  wordTranslate: string;
+}
+
+interface data {
+  rounds: {
+    levelData: {
+      author: string;
+      cutSrc: string;
+      id: string;
+      imageSrc: string;
+      name: string;
+      year: string;
+    };
+    words: {
+      audioExample: string;
+      id: number;
+      textExample: string;
+      textExampleTranslate: string;
+      word: string;
+      wordTranslate: string;
+    }[];
+  }[];
+  roundsCount: number;
+}
+
+let arrLevels: data;
+let levelData: levelData;
+const currentLevel = 1;
+const currentRound = 0;
+const currentWords = 0;
+
+function createGame() {
+  const gameBox = document.createElement('div');
+  gameBox.classList.add('game-box');
+  mainPageGame.append(gameBox);
+
+  for (let i = 0; i < 10; i += 1) {
+    const gameField = document.createElement('div');
+    gameField.classList.add('game-box__field');
+    gameField.setAttribute('data-field', String(i));
+    gameBox.append(gameField);
+  }
+}
+
+function movePuzzle(event: Event) {
+  const gameFields = document.getElementsByClassName('game-box__field');
+
+  if (event.target instanceof HTMLElement && event.target.dataset.complete !== 'true') {
+    gameFields[currentWords].append(event.target as HTMLElement);
+  }
+}
+
+function createAnswers() {
+  const gameAnswers = document.createElement('div');
+  gameAnswers.classList.add('game-answers');
+  mainPageGame.append(gameAnswers);
+
+  levelData = arrLevels.rounds[currentRound].words[currentWords];
+  const arrWords = levelData.textExample.split(' ');
+
+  arrWords.sort(() => Math.random() - 0.5);
+
+  for (let i = 0; i < arrWords.length; i += 1) {
+    const word = document.createElement('div');
+    word.classList.add('game-answers__word');
+    word.textContent = arrWords[i];
+    word.style.width = `${100 / arrWords.length}%`;
+    word.dataset.checked = 'false';
+    gameAnswers.append(word);
+    word.addEventListener('click', (event) => {
+      movePuzzle(event);
+    });
+  }
+}
+
+function getFile(link: string) {
+  fetch(link)
+    .then((response) => response.json())
+    .then((data) => {
+      arrLevels = data;
+    });
+}
+
+getFile(`lingleo/data/wordCollectionLevel${currentLevel}.json`);
+
+createGame();
+setTimeout(() => {
+  console.log(arrLevels);
+  createAnswers();
+}, 500);
+
 export { pageGame };
--- rss-puzzle/webpack.config.js
+++ rss-puzzle/webpack.config.js
@@ -4,6 +4,7 @@ const HtmlWebpackPlugin = require('html-webpack-plugin');
 const { CleanWebpackPlugin } = require('clean-webpack-plugin');
 const DotenvWebpackPlugin = require('dotenv-webpack');
 const EslingPlugin = require('eslint-webpack-plugin');
+const CopyWebpackPlugin = require('copy-webpack-plugin');
 
 const baseConfig = {
   entry: path.resolve(__dirname, './src/index.ts'),
@@ -44,6 +45,14 @@ const baseConfig = {
     }),
     new CleanWebpackPlugin(),
     new EslingPlugin({ extensions: 'ts' }),
+    new CopyWebpackPlugin({
+      patterns: [
+        {
+          from: path.resolve(__dirname, './src/lingleo'),
+          to: path.resolve(__dirname, './dist/lingleo'),
+        },
+      ],
+    }),
   ],
 };
 
