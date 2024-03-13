77f000a483e2cb09b57c082bc9c41ba451d56a2b
Yury Butskevich <bxyura@gmail.com>
2024-03-13 13:50:57 +0300

    feat: add two separate sections on results window

--- rss-puzzle/src/components/resultsWindow.ts
+++ rss-puzzle/src/components/resultsWindow.ts
@@ -7,6 +7,24 @@ const resultsPanel = document.createElement('div');
 resultsPanel.classList.add('results-window__panel');
 resultsWindow.append(resultsPanel);
 
+const dontKnowText = document.createElement('p');
+dontKnowText.innerHTML = `I don't know: <span>${0}</span>`;
+dontKnowText.classList.add('results-window__panel_text');
+resultsPanel.append(dontKnowText);
+
+const dontKnowBox = document.createElement('div');
+dontKnowBox.classList.add('results-window__panel__box');
+resultsPanel.append(dontKnowBox);
+
+const knowText = document.createElement('p');
+knowText.innerHTML = `I know: <span>${0}</span>`;
+knowText.classList.add('results-window__panel_text');
+resultsPanel.append(knowText);
+
+const knowBox = document.createElement('div');
+knowBox.classList.add('results-window__panel__box');
+resultsPanel.append(knowBox);
+
 const btnContinueInResults = document.createElement('button');
 btnContinueInResults.textContent = 'Continue';
 btnContinueInResults.classList.add('results-window__panel__button');
