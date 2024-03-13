b6bc404d8cc2d7663035560363c61adee28c28c6
Yury Butskevich <bxyura@gmail.com>
2024-03-13 12:24:25 +0300

    feat: add btn continue for results window

--- rss-puzzle/src/components/resultsWindow.ts
+++ rss-puzzle/src/components/resultsWindow.ts
@@ -3,4 +3,13 @@ import '../components/resultsWindow.css';
 const resultsWindow = document.createElement('div');
 resultsWindow.classList.add('results-window');
 
-export { resultsWindow };
+const resultsPanel = document.createElement('div');
+resultsPanel.classList.add('results-window__panel');
+resultsWindow.append(resultsPanel);
+
+const btnContinueInResults = document.createElement('button');
+btnContinueInResults.textContent = 'Continue';
+btnContinueInResults.classList.add('results-window__panel__button');
+resultsPanel.append(btnContinueInResults);
+
+export { resultsWindow, btnContinueInResults };
