5d6e77703a5e765337892cb13558e689407675e4
Yury Butskevich <bxyura@gmail.com>
2024-03-22 13:04:24 +0300

    feat: add header component and container for app in index.js

---
+++ async-race/src/components/header.ts
@@ -0,0 +1,14 @@
+import 'header.css';
+
+const header = document.createElement('header');
+header.classList.add('header');
+
+const btnToGarage = document.createElement('button');
+const btnToWinners = document.createElement('button');
+btnToGarage.classList.add('header__btn');
+btnToWinners.classList.add('header__btn');
+btnToGarage.textContent = 'To Garage';
+btnToWinners.textContent = 'To Winners';
+header.append(btnToGarage, btnToWinners);
+
+export { header, btnToGarage, btnToWinners };
--- async-race/src/index.ts
+++ async-race/src/index.ts
@@ -1,4 +1,9 @@
 import 'index.css';
+import { header, btnToGarage, btnToWinners } from './components/header';
 import { getCars } from './components/api';
+
+const app = document.createElement('div');
+app.classList.add('container');
+app.append(header);
 
 console.log(getCars(1, 4));
