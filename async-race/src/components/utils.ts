dc8b7154126a00f17031aafb3b331c491782b0f7
Yury Butskevich <bxyura@gmail.com>
2024-03-23 17:27:34 +0300

    refactor: change way for clear inputs

--- async-race/src/components/utils.ts
+++ async-race/src/components/utils.ts
@@ -1,6 +1,9 @@
 function clearFields(page: HTMLElement) {
   const inputs = page.getElementsByTagName('input');
-  for (let i = 0; i < inputs.length; i += 1) inputs[i].value = '';
+  for (let i = 0; i < inputs.length; i += 1) {
+    if (inputs[i].type === 'text') inputs[i].value = '';
+    if (inputs[i].type === 'color') inputs[i].value = '#000000';
+  }
 }
 
 export { clearFields };
