7c7b73018d4d33b73e438b6cb3d3bbfff476e7d5
Yury Butskevich <bxyura@gmail.com>
2024-04-10 15:22:15 +0300

    feat: add items and logo to footer

--- fun-chat/src/components/footer.ts
+++ fun-chat/src/components/footer.ts
@@ -1,5 +1,29 @@
 import './footer.sass';
+import '../assets/images/svg/logo-rs.svg';
 
-const footer = document.createElement('header');
+const footer = document.createElement('footer');
+footer.classList.add('footer');
+
+const footerSchoolLink = document.createElement('a');
+footerSchoolLink.href = 'https://rs.school/';
+footerSchoolLink.target = '_blank';
+footerSchoolLink.classList.add('footer__school');
+footerSchoolLink.textContent = 'RSSchool';
+
+const footerSchoolLogo = document.createElement('img');
+footerSchoolLogo.src = 'logo-rs.svg';
+footerSchoolLogo.classList.add('footer__school__img');
+footerSchoolLink.prepend(footerSchoolLogo);
+
+const footerDevLink = document.createElement('a');
+footerDevLink.href = 'https://github.com/yourunb';
+footerDevLink.classList.add('footer__developer');
+footerDevLink.innerHTML = '&copy; Yury Butskevich';
+
+const footerYear = document.createElement('p');
+footerYear.classList.add('footer__year');
+footerYear.textContent = '2024';
+
+footer.append(footerSchoolLink, footerDevLink, footerYear);
 
 export { footer };
--- fun-chat/src/index.ts
+++ fun-chat/src/index.ts
@@ -1,7 +1,8 @@
 import './index.sass';
 import { header } from './components/header';
+import { footer } from './components/footer';
 
 const main = document.createElement('main');
 main.classList.add('main');
 
-document.body.append(header, main);
+document.body.append(header, main, footer);
