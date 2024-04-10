5b7ebcbffb7ec632217e4ca001d8d1bf8c0c4335
Yury Butskevich <bxyura@gmail.com>
2024-04-10 16:24:52 +0300

    feat: add change page and change hash, add error on page 404

--- fun-chat/src/index.ts
+++ fun-chat/src/index.ts
@@ -2,4 +2,25 @@ import './index.sass';
 import { pageChat } from './pages/pageChat';
 import { pageLogin } from './pages/pageLogin';
 
-document.body.append(pageChat);
+window.addEventListener('load', () => {
+  if (localStorage.user !== undefined) {
+    location.hash = '#chat';
+    document.body.append(pageChat);
+  } else {
+    location.hash = '#login';
+    document.body.append(pageLogin);
+  }
+});
+
+window.addEventListener('hashchange', () => {
+  if (location.hash === '#login' && localStorage.user === undefined) {
+    document.body.innerHTML = '';
+    document.body.append(pageLogin);
+  }
+  if (location.hash === '#login' && localStorage.user !== undefined) {
+    location.hash = '#chat';
+  }
+  if (location.hash !== '#login' && location.hash !== '#chat') {
+    document.body.innerHTML = '<p class="error">Error 404:&nbsp;<span>This page does not exist</span><p>';
+  }
+});
