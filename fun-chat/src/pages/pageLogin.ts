ee28ebd0fe47e03d771456e546447131809cdcc8
Yury Butskevich <bxyura@gmail.com>
2024-04-10 18:03:20 +0300

    feat: add btn to for open info about app

--- fun-chat/src/pages/pageLogin.ts
+++ fun-chat/src/pages/pageLogin.ts
@@ -1,9 +1,14 @@
 import './pageLogin.sass';
+import '../assets/images/svg/info.svg';
 import { formLogin } from '../components/formLogin';
 
 const pageLogin = document.createElement('main');
 pageLogin.classList.add('page-login__main');
 
-pageLogin.append(formLogin);
+const btnInfo = document.createElement('img');
+btnInfo.classList.add('info');
+btnInfo.src = 'info.svg';
+
+pageLogin.append(formLogin, btnInfo);
 
 export { pageLogin };
