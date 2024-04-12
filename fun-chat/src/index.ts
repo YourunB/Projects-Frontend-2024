155cf4fdc0b6aff78fc74053c5f44467bccf2e9c
Yury Butskevich <bxyura@gmail.com>
2024-04-12 11:30:30 +0300

    feat: add function for openPage and change storage for checked user

--- fun-chat/src/index.ts
+++ fun-chat/src/index.ts
@@ -2,25 +2,23 @@ import './index.sass';
 import { pageChat } from './pages/pageChat';
 import { pageLogin } from './pages/pageLogin';
 
-window.addEventListener('load', () => {
-  if (localStorage.user !== undefined) {
-    document.body.append(pageChat);
-  } else {
+function openPage() {
+  document.body.innerHTML = '';
+  if (sessionStorage.user === undefined) {
+    location.hash = '#login';
     document.body.append(pageLogin);
+    return;
+  } else {
+    location.hash = '#chat';
+    document.body.append(pageChat);
+    return;
   }
+}
+
+window.addEventListener('load', () => {
+  openPage();
 });
 
 window.addEventListener('hashchange', () => {
-  document.body.innerHTML = '';
-  if (location.hash === '#login') {
-    document.body.append(pageLogin);
-    return;
-  }
-  if (location.hash === '#chat') {
-    document.body.append(pageChat);
-    return;
-  }
-  if (location.hash !== '#login' && location.hash !== '#chat') {
-    document.body.innerHTML = '<p class="error">Error 404:&nbsp;<span>This page does not exist</span><p>';
-  }
+  openPage();
 });
