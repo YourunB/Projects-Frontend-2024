25df49695f908590a3e60c4bc78fcf4e6c33bb5a
Yury Butskevich <bxyura@gmail.com>
2024-04-15 14:52:18 +0300

    feat: add btn to header

--- fun-chat/src/components/header.ts
+++ fun-chat/src/components/header.ts
@@ -3,6 +3,9 @@ import './header.sass';
 const header = document.createElement('header');
 header.classList.add('header');
 
+const headerLogoBox = document.createElement('div');
+headerLogoBox.classList.add('header-logo-box');
+
 const headerTitle = document.createElement('h1');
 headerTitle.classList.add('header__title');
 headerTitle.textContent = 'Fun Chat';
@@ -12,6 +15,11 @@ headerLogo.classList.add('header__logo');
 headerLogo.src = 'favicon.png';
 headerLogo.alt = 'Chat';
 
-header.append(headerTitle, headerLogo);
+const btnLogOut = document.createElement('button');
+btnLogOut.classList.add('header__btn');
+btnLogOut.textContent = 'LogOut';
+
+headerLogoBox.append(headerTitle, headerLogo);
+header.append(headerLogoBox, btnLogOut);
 
 export { header };
