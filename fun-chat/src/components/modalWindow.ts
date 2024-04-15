eb21f30cff4b108d6cf8cccfb9b077e8257a1688
Yury Butskevich <bxyura@gmail.com>
2024-04-15 13:27:53 +0300

    feat: add modal window for show error or message

---
+++ fun-chat/src/components/modalWindow.ts
@@ -0,0 +1,39 @@
+import './modalWindow.sass';
+
+const modalWindow = document.createElement('div');
+modalWindow.classList.add('modal');
+
+const modalForm = document.createElement('div');
+modalForm.classList.add('modal__form');
+
+const modalFormTitle = document.createElement('h3');
+modalFormTitle.classList.add('modal__form__title');
+modalFormTitle.id = 'modal-form-title';
+modalFormTitle.textContent = 'Error';
+
+const modalFormText = document.createElement('p');
+modalFormText.classList.add('modal__form__text');
+modalFormText.id = 'modal-form-text';
+modalFormText.textContent = 'asdasdasd asd sad asdasdasd asd asd asd asd asd asd aswd sa d';
+
+const modalFormBtn = document.createElement('button');
+modalFormBtn.classList.add('modal__form__btn');
+modalFormBtn.id = 'modal-form-btn';
+modalFormBtn.textContent = 'OK';
+
+modalForm.append(modalFormTitle, modalFormText, modalFormBtn);
+modalWindow.append(modalForm);
+
+function closeModal() {
+  modalWindow.classList.remove('modal_show');
+}
+
+modalFormBtn.addEventListener('click', () => {
+  closeModal();
+});
+
+modalWindow.addEventListener('click', () => {
+  closeModal();
+});
+
+export { modalWindow, modalFormTitle, modalFormText };
--- fun-chat/src/pages/pageLogin.ts
+++ fun-chat/src/pages/pageLogin.ts
@@ -4,11 +4,12 @@ import { formLogin, btnLogin, inputName, inputPass } from '../components/formLog
 import { infoApp, btnInfo } from '../components/infoApp';
 import { apiAuthorization } from '../components/apiChat';
 import { v4 as uuidv4 } from 'uuid';
+import { modalWindow, modalFormText, modalFormTitle } from '../components/modalWindow';
 
 const pageLogin = document.createElement('main');
 pageLogin.classList.add('page-login__main');
 
-pageLogin.append(formLogin, btnInfo, infoApp);
+pageLogin.append(formLogin, btnInfo, infoApp, modalWindow);
 
 btnLogin.addEventListener('click', (event) => {
   event.preventDefault();
@@ -20,6 +21,11 @@ btnLogin.addEventListener('click', (event) => {
       if (obj.type !== 'ERROR') {
         addUserToSessionStorage(obj.id, obj.payload.user.login);
         location.hash = '#chat';
+      }
+      if (obj.type === 'ERROR') {
+        modalWindow.classList.add('modal_show');
+        modalFormText.textContent = obj.payload.error;
+        modalFormTitle.textContent = obj.type;
       }
     }
   });
