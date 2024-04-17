78923a31639fae99ee5a3f17f2d216c3a5f818b6
Yury Butskevich <bxyura@gmail.com>
2024-04-17 15:44:16 +0300

    feat: add search in chat component

--- fun-chat/src/components/chat.ts
+++ fun-chat/src/components/chat.ts
@@ -10,7 +10,8 @@ const chatSearch = document.createElement('input');
 chatSearch.classList.add('chat__search');
 chatSearch.placeholder = 'Search';
 
-chat.append(chatSearch, chatUsersBox);
+chatUsersBox.append(chatSearch);
+chat.append(chatUsersBox);
 
 const chatUsersBoxActive = document.createElement('div');
 const chatUsersBoxInactive = document.createElement('div');
@@ -25,5 +26,18 @@ function addUserToChat(userName: string, isLogined: boolean) {
   if (isLogined) chatUsersBoxActive.append(user);
   else chatUsersBoxInactive.append(user);
 }
+
+function searchUser(value = '') {
+  const arrUsers = chat.getElementsByClassName('chat__users__user');
+  for (let i = 0; i < arrUsers.length; i += 1) {
+    if (arrUsers[i].textContent?.toLowerCase().indexOf(value.toLocaleLowerCase()) === -1) {
+      arrUsers[i].classList.add('chat__users__user_hide');
+    } else arrUsers[i].classList.remove('chat__users__user_hide');
+  }
+}
+
+chatSearch.addEventListener('input', () => {
+  searchUser(chatSearch.value);
+});
 
 export { chat, chatUsersBox, addUserToChat, chatUsersBoxActive, chatUsersBoxInactive };
